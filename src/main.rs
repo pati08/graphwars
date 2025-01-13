8de362033a1a6e9aaa88da517771ff32b3b8265f
Patrick Oberholzer <patrickoberholzer08@gmail.com>
2025-01-13 10:14:54 -0500

    working on stuff

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,79 @@
+use std::{num::NonZeroU8, time::Duration};
+
 use bevy::{prelude::*, window::WindowResized};
 use bevy_egui::{
     EguiContexts, EguiPlugin,
     egui::{self, Id},
 };
+
+enum PlayerSelect {
+    Player1,
+    Player2,
+}
+
+#[derive(Resource)]
+enum GamePhase {
+    Setup(SetupPhase),
+    Playing(PlayPhase),
+    GameFinished(FinishedPhase),
+}
+
+impl Default for GamePhase {
+    fn default() -> Self {
+        Self::Setup(SetupPhase {
+            player_1: PlayerConfig {
+                soldier_num: NonZeroU8::new(1).unwrap(),
+                name: "Player 1".to_string(),
+            },
+            player_2: PlayerConfig {
+                soldier_num: NonZeroU8::new(1).unwrap(),
+                name: "Player 2".to_string(),
+            },
+            turn_seconds: 60,
+        })
+    }
+}
+
+struct SetupPhase {
+    player_1: PlayerConfig,
+    player_2: PlayerConfig,
+    turn_seconds: u32,
+}
+
+struct PlayerConfig {
+    soldier_num: NonZeroU8,
+    name: String,
+}
+
+struct PlayPhase {
+    turn_time: Duration,
+    player_1: PlayerState,
+    player_2: PlayerState,
+    turn: PlayerSelect,
+    turn_eq: String,
+}
+struct PlayerState {
+    name: String,
+    living_soldiers: Vec<Soldier>,
+}
+
+#[derive(Component)]
+struct Soldier {
+    player: PlayerSelect,
+    id: u8,
+    graph_location: Vec2,
+}
+#[derive(Bundle)]
+struct SoldierBundle {
+    soldier: Soldier,
+    transform: Transform,
+    mesh: Mesh2d,
+    material: MeshMaterial2d<ColorMaterial>,
+}
+
+struct FinishedPhase {
+    winner: PlayerSelect,
+}
 
 fn main() {
     App::new()
@@ -14,6 +85,7 @@ fn main() {
             keyboard_captured: false,
             pointer_captured: false,
         })
+        .insert_resource(GamePhase::default())
         // Systems that create Egui widgets should be run during the `Update` Bevy schedule,
         // or after the `EguiPreUpdateSet::BeginPass` system (which belongs to the `PreUpdate` Bevy schedule).
         .add_systems(Update, ui_system)
@@ -45,10 +117,9 @@ fn draw_graph(mut gizmos: Gizmos) {
 
 fn ui_system(
     mut contexts: EguiContexts,
-    window: Single<&Window>,
     mut app_exit_events: ResMut<Events<AppExit>>,
+    mut state: ResMut<GamePhase>,
 ) {
-    egui::Window::new("Controls").show(contexts.ctx_mut(), |ui| {});
     egui::containers::panel::TopBottomPanel::top(Id::new("top_panel")).show(
         contexts.ctx_mut(),
         |ui| {
@@ -57,6 +128,27 @@ fn ui_system(
             }
         },
     );
+    match *state {
+        GamePhase::Setup(_) => setup_ui(contexts.ctx_mut(), &mut state),
+        GamePhase::Playing(_) => play_ui(contexts.ctx_mut(), &mut state),
+        GamePhase::GameFinished(_) => finished_ui(contexts.ctx_mut(), &mut state),
+    };
+}
+
+fn setup_ui(mut context: &mut bevy_egui::egui::Context, mut state: &mut GamePhase) {
+    let &mut GamePhase::Setup(ref mut setup_state) = state else {
+        return;
+    };
+}
+fn play_ui(mut context: &mut bevy_egui::egui::Context, mut state: &mut GamePhase) {
+    let &mut GamePhase::Playing(ref mut playing_state) = state else {
+        return;
+    };
+}
+fn finished_ui(mut context: &mut bevy_egui::egui::Context, mut state: &mut GamePhase) {
+    let &mut GamePhase::GameFinished(ref mut finished_state) = state else {
+        return;
+    };
 }
 
 fn capture_info(mut input_capture_state: ResMut<InputCaptureState>, mut egui: EguiContexts) {
@@ -77,9 +169,9 @@ fn setup(
 ) {
     commands.spawn(Camera2d);
 
-    commands.spawn((
-        Mesh2d(meshes.add(Rectangle::new(440., 440.))),
-        MeshMaterial2d(materials.add(Color::WHITE)),
-        Transform::default(),
-    ));
+    // commands.spawn((
+    //     Mesh2d(meshes.add(Rectangle::new(440., 440.))),
+    //     MeshMaterial2d(materials.add(Color::WHITE)),
+    //     Transform::default(),
+    // ));
 }
