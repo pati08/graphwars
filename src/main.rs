e57d3218a434f2170f8f468911152b705efb6cc5
Patrick Oberholzer <patrickoberholzer08@gmail.com>
2025-01-13 10:15:49 -0500

    stuff

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,8 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugins(EguiPlugin)
+        // .insert_resource(ClearColor(Color::srgb(0.9, 0.9, 0.9)))
         .insert_resource(Time::new(std::time::Instant::now()))
-        .insert_resource(TextMoveDistance(0.0))
-        .insert_resource(IsMoving(false))
-        .insert_resource(EditorText("".to_string()))
         .insert_resource(InputCaptureState {
             keyboard_captured: false,
             pointer_captured: false,
@@ -21,40 +19,36 @@ fn main() {
         .add_systems(Update, ui_system)
         .add_systems(Update, capture_info)
         .add_systems(Startup, setup)
-        .add_systems(Update, move_text)
-        .add_systems(Update, on_resize)
-        .add_systems(Update, space_toggle.after(capture_info))
+        .add_systems(Update, draw_graph)
         .run();
 }
 
-#[derive(Resource)]
-struct TextMoveDistance(f32);
-
-#[derive(Resource)]
-struct IsMoving(bool);
-
-#[derive(Resource)]
-struct EditorText(String);
+fn draw_graph(mut gizmos: Gizmos) {
+    gizmos.grid_2d(
+        Isometry2d::default(),
+        UVec2::new(20, 20),
+        Vec2::new(20., 20.),
+        Color::BLACK,
+    );
+    const RES: f32 = 0.01;
+    gizmos.linestrip_2d(
+        (0..(5. / RES).round() as usize)
+            .map(|i| i as f32 * RES)
+            .map(|i| Vec2 {
+                x: i * 20.,
+                y: i * i * 20.,
+            })
+            .take_while(|i| i.x.abs() <= 200. && i.y.abs() <= 200.),
+        Color::srgb(1., 0., 0.),
+    );
+}
 
 fn ui_system(
     mut contexts: EguiContexts,
-    mut is_moving: ResMut<IsMoving>,
-    mut text: Query<&mut Transform, With<HelloWorldText>>,
-    mut moved: ResMut<TextMoveDistance>,
-    mut editortext: ResMut<EditorText>,
     window: Single<&Window>,
     mut app_exit_events: ResMut<Events<AppExit>>,
 ) {
-    egui::Window::new("Controls").show(contexts.ctx_mut(), |ui| {
-        ui.checkbox(&mut is_moving.0, "Move text");
-        if ui.button("Reset").clicked() {
-            moved.0 = 0.;
-            for mut i in text.iter_mut() {
-                i.translation.x = -window.size().x / 2.;
-            }
-        }
-        ui.text_edit_multiline(&mut editortext.0);
-    });
+    egui::Window::new("Controls").show(contexts.ctx_mut(), |ui| {});
     egui::containers::panel::TopBottomPanel::top(Id::new("top_panel")).show(
         contexts.ctx_mut(),
         |ui| {
@@ -76,57 +70,16 @@ struct InputCaptureState {
     pointer_captured: bool,
 }
 
-fn setup(mut commands: Commands, window: Single<&Window>) {
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
     commands.spawn(Camera2d);
-    println!("Window width: {}", window.size().x);
-    commands.spawn((Text2d::new("Hello, world"), HelloWorldText, Transform {
-        translation: Vec3 {
-            x: -window.size().x / 2.,
-            y: 0.,
-            z: 0.,
-        },
-        rotation: Quat::default(),
-        scale: Vec3::ONE,
-    }));
-}
 
-fn space_toggle(
-    keys: Res<ButtonInput<KeyCode>>,
-    mut is_moving: ResMut<IsMoving>,
-    input_capture_state: Res<InputCaptureState>,
-) {
-    if !input_capture_state.keyboard_captured && keys.just_pressed(KeyCode::Space) {
-        is_moving.0 = !is_moving.0;
-    }
-}
-
-fn on_resize(
-    mut text: Query<&mut Transform, With<HelloWorldText>>,
-    mut resize_reader: EventReader<WindowResized>,
-    moved: Res<TextMoveDistance>,
-) {
-    for event in resize_reader.read() {
-        for mut i in text.iter_mut() {
-            i.translation.x = -event.width / 2. + moved.0;
-        }
-    }
-}
-
-#[derive(Component)]
-struct HelloWorldText;
-
-fn move_text(
-    mut text: Query<&mut Transform, With<HelloWorldText>>,
-    time: Res<Time>,
-    mut moved: ResMut<TextMoveDistance>,
-    is_moving: Res<IsMoving>,
-) {
-    if !is_moving.0 {
-        return;
-    }
-    for mut i in text.iter_mut() {
-        let move_dist = 100. * time.delta_secs_f64() as f32;
-        i.translation.x += move_dist;
-        moved.0 += move_dist;
-    }
+    commands.spawn((
+        Mesh2d(meshes.add(Rectangle::new(440., 440.))),
+        MeshMaterial2d(materials.add(Color::WHITE)),
+        Transform::default(),
+    ));
 }
